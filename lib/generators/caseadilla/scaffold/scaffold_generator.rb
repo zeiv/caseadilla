3886102b85e9fa7c276db82d051df03f61796e36
Xavier Bick <fxb9500@gmail.com>
2015-05-29 14:21:06 -0500

    Updates top menu to reflect user's time zone (fixes #13), change default form type for text data type to be text_area_wysiwyg.

--- lib/generators/caseadilla/scaffold/scaffold_generator.rb
+++ lib/generators/caseadilla/scaffold/scaffold_generator.rb
@@ -1,14 +1,14 @@
 module Caseadilla
   class ScaffoldGenerator < Rails::Generators::NamedBase
-  
+
     include Caseadilla::CaseadillaHelper
     include Rails::Generators::Migration
     source_root File.expand_path('../templates', __FILE__)
-  
+
     argument :attributes, :type => :array, :required => true, :desc => "attribute list required"
-    
+
     class_options :create_model => false, :read_only => false, :no_index => false
-  
+
     def self.next_migration_number dirname
       if ActiveRecord::Base.timestamped_migrations
         Time.now.utc.strftime("%Y%m%d%H%M%S")
@@ -16,7 +16,7 @@ module Caseadilla
         "%.3d" % (current_migration_number(dirname) + 1)
       end
     end
-  
+
     def generate_files
       @plural_route = (plural_name != singular_name) ? plural_name : "#{plural_name}_index"
       @read_only = options[:read_only]
@@ -28,19 +28,19 @@ module Caseadilla
       template 'views/new.html.erb', "app/views/caseadilla/#{plural_name}/new.html.erb" unless @read_only
       template 'views/_form.html.erb', "app/views/caseadilla/#{plural_name}/_form.html.erb"
       template 'views/_table.html.erb', "app/views/caseadilla/#{plural_name}/_table.html.erb"
-      
+
       add_namespace_to_routes
       add_to_routes
       add_to_navigation unless @no_index
-      
+
       if options[:create_model]
         template 'model.rb', "app/models/#{singular_name}.rb"
         migration_template 'migration.rb', "db/migrate/create_#{plural_name}.rb"
       end
     end
-  
+
   protected
-  
+
     #replacements for standard Rails generator route. This one only adds once
     def add_namespace_to_routes
       puts "   caseadilla     adding namespace to routes.rb"
@@ -51,7 +51,7 @@ module Caseadilla
         gsub_add_once plural_name, file_to_update, "\n\t#Caseadilla routes\n\t" + line_to_add + "\n\tend\n", insert_sentinel
       end
     end
-    
+
     def add_to_routes
       puts "   caseadilla     adding #{plural_name} resources to routes.rb"
       file_to_update = Rails.root + 'config/routes.rb'
@@ -77,7 +77,7 @@ module Caseadilla
       insert_sentinel = '<!-- SCAFFOLD_INSERT -->'
       gsub_add_once plural_name, file_to_update, line_to_add, insert_sentinel
     end
-  
+
     def gsub_add_once m, file, line, sentinel
       unless options[:pretend]
         gsub_file file, /(#{Regexp.escape("\n#{line}")})/mi do |match|
@@ -88,12 +88,12 @@ module Caseadilla
         end
       end
     end
-    
+
     def gsub_file(path, regexp, *args, &block)
       content = File.read(path).gsub(regexp, *args, &block)
       File.open(path, 'wb') { |file| file.write(content) }
     end
-  
+
     def field_type(type)
       case type.to_s.to_sym
         when :integer, :float, :decimal   then :text_field
@@ -101,11 +101,11 @@ module Caseadilla
         when :time, :timestamp            then :time_select
         when :datetime                    then :datetime_select
         when :string                      then :text_field
-        when :text                        then :text_area
+        when :text                        then :text_area_wysiwyg
         when :boolean                     then :check_box
       else
         :text_field
-      end      
+      end
     end
   end
 end
