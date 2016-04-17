4de8570106b4a8d38224fd2b4c4ca0c87f616666
Xavier Bick <fxb9500@gmail.com>
2016-04-17 12:47:07 -0500

    Actually *require* non-stupid-digest-assets

--- lib/caseadilla.rb
+++ lib/caseadilla.rb
@@ -3,6 +3,7 @@ require "caseadilla/version"
 if defined?(Rails) && Rails::VERSION::MAJOR == 4
 	require 'caseadilla/engine'
 	require 'will_paginate'
+	require 'non-stupid-digest-assets'
 	if Caseadilla::Engine.flavor = :steak
 		require 'devise'
 		require 'declarative_authorization'
--- lib/caseadilla/version.rb
+++ lib/caseadilla/version.rb
@@ -1,3 +1,3 @@
 module Caseadilla
-  VERSION = "0.0.16"
+  VERSION = "0.0.17"
 end
